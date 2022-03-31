6e86b9021472be9c3ac38b35b791523ff4c27105
christian calleja <christiandcalleja@yahoo.com.ar>
2022-03-31 09:58:39 -0300

    librerias cargadas

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,17 @@
 function App() {
   return (
     <div>
-
+{/* ToDoList
+Navbar (Rodrigo)
+Hero / Banner (Mileidy)
+Productos (Christian)
+Detalles / particularidades (Christian)
+Seccion -> Detalles e imágenes (Santiago)
+Seccion -> Detalles e imágenes 2 (Mileidy)
+Descanso (Rodrigo)
+Carousel (Santiago)
+Redes (Berta)
+Footer (Berta) */}
     </div>
   );
 }
