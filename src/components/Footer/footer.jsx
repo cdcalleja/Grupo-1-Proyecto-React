1a51ace3f27fcab9c59a1246f6ccb984314e6ab1
Berta Macina <bmacina2018@gmail.com>
2022-04-13 17:13:43 -0300

    Berta Cambio 1

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,51 @@
+import './footer.css'
+import React from 'react';
+
+const Footer = () => {
+return(
+<div className='main-footer'
+    <div className='container'>
+            <div className='row'>
+                {/* {Colum 1} */}
+                <div className='col'>
+                    <h4.ESTILOS S.A.</h4>
+                    <ul className='list-unstyled'>
+                        <li 1000-1000</li>
+                        <li Buenos Aires></li>
+                        <li Argentina></li>
+                        <li Email></li>
+                    </ul>
+                </div>
+                {/* {Colum2} */}
+                <div className='col'>
+                    <h4>Consultas</h4>
+                    <ul className='list-unstyled'>
+                        <li Productos></li>
+                        <li La empresa></li>
+                        <li Entregas Reclamos></li>
+                    </ul>
+                </div>
+                {/* {Columna 3} */}
+                <div className='col'>
+                    <h4>Redes Sociales</h4>
+                    < className='list-unstyled'>
+               <i className="bi bi-facebook"></i>
+//             <i className="bi bi-instagram"></i>
+//             <i className="bi bi-twitter"></i>
+//             <i className="bi bi-youtube"></i>
+//             <i className="bi bi-whatsapp"></i>
+//             <i className="bi bi-chat"></i>
+//             </li>    
+                </ul>
+                </div>
+                </hr>
+                <div className='row'>
+                    <p className='col-sm'>
+                        &copy;{new Date().getFullYear()} ESTILOS S.A. | Derechos Reservados ! Terminos de Servicios ! Privacidad 
+                    </p>
+                </div>
+</div>
+</div>
+</div>
+
+export default Footer
